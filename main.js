3ecfd4efd09055c63c260ba9f79e0a257bede580
Daniel <daniel-2193@hotmail.com>
2023-06-27 23:26:40 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+//
+$(document).ready(function(){
+    $('.title__container-btn').click(function(){
+        $('.form').slideToggle();
+    })
+
+    $('.cancel').click(function(){
+        $('.form').slideToggle();
+    })
+})
